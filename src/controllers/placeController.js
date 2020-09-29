ec699c497e27870b5353434a21f1c0a402b593b8
ahmed sakli bouhlel <ahmed.sakli-bouhlel@estiam.com>
2020-09-29 21:35:11 +0200

    controlleur et route de getplacebyuser

--- src/controllers/placeController.js
+++ src/controllers/placeController.js
@@ -1,16 +1,16 @@
 const Place = require("../models/place");
-const validationHandler = require("../validations/validationHandler")
+const validationHandler = require("../validations/validationHandler");
 
 exports.show = async (req, res, next) => {
-    try {
-      const place = await Place.findOne({ _id: req.params.id });
-      res.json(place)
-      
-      //res.render("place", {places: places});
-    } catch (error) {
-      next(error);
-    }
-  };
+  try {
+    const place = await Place.findOne({ _id: req.params.id });
+    res.json(place);
+
+    //res.render("place", {places: places});
+  } catch (error) {
+    next(error);
+  }
+};
 
 //   manque con  : Est ce qu'il faut mettre avant try catch
 //       if(!existingPlace){
@@ -19,75 +19,81 @@ exports.show = async (req, res, next) => {
 //       throw error;
 //     }
 
-exports.add =(req, res) => {    
-    res.render("new");
+exports.add = (req, res) => {
+  res.render("new");
 };
 
 exports.store = async (req, res, next) => {
-    try {
-      validationHandler(req);
-      let place = new Place(req.body);
-      place = await place.save();
-      //res.json(place);
-      res.status(200).render('storeplace',{place})
-    } catch (error) {
-      next(error);
+  try {
+    validationHandler(req);
+    let place = new Place(req.body);
+    place = await place.save();
+    //res.json(place);
+    res.status(200).render("storeplace", { place });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//manque validators
+
+exports.patch = async (req, res) => {
+  try {
+    // console.log(req.params)
+    const place = await Place.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        data: place,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+//manque code scénari
+
+exports.all = async (req, res, next) => {
+  try {
+    const places = await Place.find({});
+    console.log(places);
+    res.render("overview", { places: places });
+    //res.json(places)
+  } catch (error) {
+    next(error);
+  }
+};
+exports.delete = async (req, res, next) => {
+  try {
+    const place = await Place.findById(req.body.id);
+    if (!place) {
+      const error = new Error("La place demandé n'existe pas");
+      error.statusCode = 404;
+      throw error;
     }
-  };
-
-  //manque validators
-
-  exports.patch = async (req, res) => {
-
-        try {
-            // console.log(req.params)
-            const place = await Place.findByIdAndUpdate(req.params.id, req.body, {
-                new: true,
-                runValidators: true
-            });
-    
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    data: place
-                }
-            });
-        } catch (err) {
-            res.status(400).json({
-                status: 'fail',
-                message: err
-            });
-        }
-    };
-
-    //manque code scénari
-
-    exports.all = async (req, res, next) => {
-        try {
-          const places = await Place.find({
-            
-          });
-          console.log (places)
-          res.render("overview", { places: places });
-          //res.json(places)
-        } catch (error) {
-          next(error);
-        }
-      };
-      exports.delete = async(req, res ,next) => {
-        try{
-          
-          const place = await Place.findById(req.body.id);
-          if(!place){
-              const error = new Error("La place demandé n'existe pas");
-              error.statusCode = 404;
-              throw error;
-          }
-          await Place.deleteOne(place) ;
-          res.status(201).json({
-              status: 'success'
-          }) ;
-      }catch(error){
-          next(error) ;
-      }
-      }
+    await Place.deleteOne(place);
+    res.status(201).json({
+      status: "success",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.getPlaceByUser = async (req, res, next) => {
+  try {
+    const places = await Place.find({
+      user: req.params.id,
+    });
+    res.status(200).json(places);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/place.js
+++ src/routes/place.js
@@ -2,11 +2,22 @@ const express = require("express");
 const router = express.Router();
 const placeController = require("../controllers/placeController");
 const passportJWT = require("../middlewares/passportJWT")();
-const {hasName, hasDescription ,hasMax_guests , hasRooms , hasBathrooms} = require('../validations/validators');
+const {
+  hasName,
+  hasDescription,
+  hasMax_guests,
+  hasRooms,
+  hasBathrooms,
+} = require("../validations/validators");
 router.get("/:id/", placeController.show);
-router.get("/new",passportJWT.authenticate(),placeController.add)
-router.post("/new",[hasName, hasDescription ,hasMax_guests , hasRooms ,hasBathrooms],placeController.store);
-router.patch("/:id",passportJWT.authenticate(),placeController.patch);
-router.get("",placeController.all);
-router.post("/delete",placeController.delete);
+router.get("/new", passportJWT.authenticate(), placeController.add);
+router.post(
+  "/new",
+  [hasName, hasDescription, hasMax_guests, hasRooms, hasBathrooms],
+  placeController.store
+);
+router.patch("/:id", passportJWT.authenticate(), placeController.patch);
+router.get("", placeController.all);
+router.post("/delete", placeController.delete);
+router.get("/place/:id", placeController.getPlaceByUser);
 module.exports = router;
